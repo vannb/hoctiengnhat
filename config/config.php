9671c8d1153b891a3310f9754e2390f8b2dec2ca
vannb <baovan1993@gmail.com>
2014-05-15 23:42:56 +0700

    first commit

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
-define('DEBUG_MODE', 1);
+define('DEBUG_MODE', 0);
 define('URL',"http://localhost/hoctiengnhat/index.php?url=");
 define('PATH',"http://localhost/hoctiengnhat/");
 define('DEFAULT_BRAND', '2200');
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'sell');
-define('DB_USER', 'root');
-define('DB_PASS', 'VanNB');
+define('DB_HOST', '183.91.14.174');
+define('DB_NAME', 'share4v_vannb');
+define('DB_USER', 'share4v_vannb');
+define('DB_PASS', 'vannb');
--- views/logout/index.php
+++
@@ -1,5 +0,0 @@
-<center>
-    Are you sure want to logout?</br>
-    <a href = '<?php echo URL?>Logout/LogoutSubmit'><button>Yes</button><a>
-    <a href = 'javascript:history.go(-1);'><button>Nah</button><a>
-</center>
