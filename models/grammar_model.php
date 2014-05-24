e7aef58fa93b492552a6ecc6f0b2a16073376b30
vannb <baovan1993@gmail.com>
2014-05-24 17:21:31 +0700

    security

--- models/grammar_model.php
+++ models/grammar_model.php
@@ -1,10 +1,15 @@
+<?php defined('SERVER_ROOT') or die('No direct script access allowed'); ?>
 <?php
-class grammar_Model extends Model{
+
+class grammar_Model extends Model {
+
     public function __construct() {
         parent::__construct();
     }
-    function qry_lesson_grammar($lesson_id){
-       $result = DB::search('t_grammar',array(),array('FK_LESSON' => $lesson_id));
+
+    function qry_lesson_grammar($lesson_id) {
+        $result = DB::search('t_grammar', array(), array('FK_LESSON' => $lesson_id));
         return $result;
-}
+    }
+
 }
