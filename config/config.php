174493549785f1f0b730681e701e83e9be8e9a3a
Loving <Loving@VanNB>
2014-07-15 15:47:31 +0700

    config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DEBUG_MODE', 1);
+define('DEBUG_MODE',0);
 define('URL',"http://localhost/hoctiengnhat/index.php?url=");
 define('PATH',"http://localhost/hoctiengnhat/");
 define('DEFAULT_BRAND', '2200');
@@ -8,3 +8,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'hoctiengnhat');
 define('DB_USER', 'VanNB');
 define('DB_PASS', 'VanNB');
+define('DEFAULT_ROWS_PER_PAGE', 10);
